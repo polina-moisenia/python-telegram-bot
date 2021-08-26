1e76ac505b8f17440d95f948a2f763254fcc5f52
Polina Moisenia <p.moysenia@gmail.com>
2021-08-26 15:26:03 +0300

    Words filter

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
 import os
 import telebot
+import re
 
 API_KEY = os.environ['TELEGRAM_API_KEY']
 bot = telebot.TeleBot(token=API_KEY)
 
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=['start']) # welcome message handler
 def send_welcome(message):
     bot.reply_to(message, 'Welcome, honey :)')
+
+@bot.message_handler(commands=['help']) # help message handler
+def send_welcome(message):
+    bot.reply_to(message, 'This bot is searching for specific words...')
+
+@bot.message_handler(func=lambda msg: msg.text is not None)
+def at_converter(message):
+    msg = re.split("\W+", message.text)
+    temp_list = []
+    for i in msg:
+        if len(i) == 4:
+            temp_list.append(i.lower())
+    if temp_list:
+        bot.reply_to(message, "I found the following words: " + " ".join(temp_list))
+    if not temp_list:
+        bot.reply_to(message, "I haven't found any words")
 
 bot.polling()
